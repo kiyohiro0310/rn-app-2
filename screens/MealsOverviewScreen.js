ddef39e8f8e11e7c77daa955d052f0cbd13d50f8
kiyo <kiyohiro.0310@gmail.com>
2022-12-24 00:24:01 +1100

    screenOptions in Stack.Navigator

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,36 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList, ImageEditor } from "react-native";
+
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 
 function MealsOverviewScreen({ route }) {
-    const catId = route.params.categoryId;
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+        title: item.title,
+        imageUrl: item.imageUrl,
+        affordability: item.affordability,
+        complexity: item.complexity,
+        duration: item.duration
+    }
+    return (
+      <MealItem {...mealItemProps}  />
+    );
+  }
   return (
     <View style={sytles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
